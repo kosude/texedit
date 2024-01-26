f44cc25577a37aa59d8b15172491680ce146f5b8
Jack Bennett <jackbennett0708@gmail.com>
2024-01-26 20:00:51 +0000

    Add the frontend as a C# project (dotnet - .csproj)

---
+++ src/frontend/Program.cs
@@ -0,0 +1,14 @@
+/*
+ *   Copyright (c) 2024 Jack Bennett.
+ *   All Rights Reserved.
+ *
+ *   See the LICENCE file for more information.
+ */
+
+namespace TexEdit {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello TexEdit");
+        }
+    }
+}
