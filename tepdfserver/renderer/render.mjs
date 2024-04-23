da01274fd2fe3190e53666048d7ede6531b9a7b1
Jack Bennett <jackbennett0708@gmail.com>
2024-04-23 09:38:45 +0100

    Add PDF server CSS to scale PDF to viewport size

--- tepdfserver/renderer/render.mjs
+++ tepdfserver/renderer/render.mjs
@@ -16,8 +16,7 @@ pdfjsLibPromise.then((pdfjsLib) => {
     loadingTask.promise.then((pdf) => {
         var pageNumber = 1;
         pdf.getPage(pageNumber).then((page) => {
-            const scale = 1.5;
-            const viewport = page.getViewport({ scale });
+            const viewport = page.getViewport({ scale: 2 });
             const outputScale = window.devicePixelRatio || 1;
 
             const canvas = document.getElementById("pdf-render");
@@ -25,17 +24,17 @@ pdfjsLibPromise.then((pdfjsLib) => {
 
             canvas.width = Math.floor(viewport.width * outputScale);
             canvas.height = Math.floor(viewport.height * outputScale);
-            canvas.style.width = Math.floor(viewport.width) + "px";
-            canvas.style.height = Math.floor(viewport.height) + "px";
+            canvas.style.width = Math.floor(viewport.width * outputScale) + "px";
+            canvas.style.height = Math.floor(viewport.height * outputScale) + "px";
 
             const transform = outputScale !== 1
-            ? [outputScale, 0, 0, outputScale, 0, 0]
-            : null;
+                ? [outputScale, 0, 0, outputScale, 0, 0]
+                : null;
 
             const renderContext = {
                 canvasContext: context,
-                transform,
-                viewport,
+                viewport: viewport,
+                transform: transform
             };
             var renderTask = page.render(renderContext);
             renderTask.promise.then(() => {
